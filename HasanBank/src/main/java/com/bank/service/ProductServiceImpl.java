8390c84fe351e2678ac42d5db168e775b773c5ea
aeonpatriot <has8ultimate@gmail.com>
2025-07-24 18:53:50 +0800

    commit for bank code

---
+++ HasanBank/src/main/java/com/bank/service/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bank.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bank.entity.ProductEntity;
+import com.bank.repo.IProductRepo;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class ProductServiceImpl implements IProductService {
+
+	private final IProductRepo productRepo;
+	
+    @Override
+    public List<ProductEntity> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    @Override
+    public ProductEntity getProductById(Long id) {
+        return productRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Product not found"));
+    }
+
+    @Override
+    public ProductEntity createProduct(ProductEntity product) {
+        return productRepo.save(product);
+    }
+
+    @Override
+    public ProductEntity updateProduct(Long id, ProductEntity updatedProduct) {
+    	ProductEntity existing = getProductById(id);
+        existing.setDescription(updatedProduct.getDescription());
+        existing.setProductName(updatedProduct.getProductName());
+        return productRepo.save(existing);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+    	productRepo.deleteById(id);
+    }
+}
